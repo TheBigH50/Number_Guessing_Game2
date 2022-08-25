da885c053d750e753fc268f2c42ab17bdf9af7b0
TheBigH50 <HFangmeier@outlook.com>
2022-08-25 13:50:22 -0500

    write some notes planning project

---
+++ app.js
@@ -0,0 +1,9 @@
+//welcome screen/start game (main screen, pose question give 2 buttons yes or no)
+
+//make no pop to thanks for visiting page
+
+//make yes pose difficulty choice with 4 buttons
+
+//connect choice to game of choice
+
+//pop option to play again with yes or no buttons (modal?)
