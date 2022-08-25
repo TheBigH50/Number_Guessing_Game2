b4f7a0a7682006632f73fce185a243d2ef4adbb2
TheBigH50 <HFangmeier@outlook.com>
2022-08-25 15:24:01 -0500

    break time

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 //welcome screen/start game (main screen, pose question give 2 buttons yes or no)
 let startGame = document.getElementById("start");
-letnoThanks = document.getElementById("noThanks");
+let noThanks = document.getElementById("noThanks");
+let thankYou = document.getElementById("thankYouCard");
+let wouldYou = document.getElementById("wouldYou");
+let gameCard = document.getElementById("gameCard");
 
 //make no pop to thanks for visiting page
+visible = function() {wouldYou.style.display = "none"};
+
+noThanks.onclick = function () {
+  wouldYou.classList.toggle(visible);
+  gameCard.style.display = "none";
+  thankYou.style.display = "flex";
+};
 
 //make yes pose difficulty choice with 4 buttons
 
