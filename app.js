ccfa4aac44f47a1e0b694789fff3800c7d5ac5c0
TheBigH50 <HFangmeier@outlook.com>
2022-08-29 08:23:37 -0500

    work on easy more

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ let difficultyOption = document.getElementById("difficultyOption");
 let easy = document.getElementById("easy");
 let hard = document.getElementById("hard");
 let overkill = document.getElementById("overkill");
+let judgement = document.getElementById("judgement");
+judgeLabel = document.getElementById("judgeLabel");
+let tryAgain = document.getElementById("tryAgain");
 //make no pop to thanks for visiting page
 async function hideContainers() {
   wouldYou.classList.toggle("hide");
@@ -33,14 +36,21 @@ startGame.addEventListener("click", (event) => {
 
 async function playGameEasy() {
   var rand = Math.floor(Math.random() * 10) + 1;
-  gameCardLabel.innerText=`I'm Thinking of a Number from 1 to 10...`;
+  gameCardLabel.innerText = `I'm Thinking of a Number from 1 to 10...`;
   let guess;
-  do {
+  makeGuess.addEventListener("click", (event) => {
     guess = guessInput.value;
+    do {
     if (guess < rand) {
-      alert(`${guess} is too low`);
+      judgeLabel.innerText = `${guess} is too low`;
+      judge(); tryAgain.addEventListener("click", (event) => {
+        guessInput.value = ""; judge();
+      });
     } else if (guess > rand) {
-      alert(`${guess} is too high`);
+      judgeLabel.innerText = `${guess} is too `;
+      judge(); tryAgain.addEventListener("click", (event) => {
+        guessInput.value = ""; judge();
+      });
     } else if (isNaN(guess)) {
       alert(`Guess a number please`);
     } else {
@@ -48,16 +58,20 @@ async function playGameEasy() {
       console.log(
         `WAY TO GO YOU GUESSED THE NUMBER ${guess} YOU ARE SO SMART!`
       );
-    }
-  } while (rand != guess);
+    }} while (guess != rand);
+  });
 }
 async function playGame() {
   difficultyOption.classList.toggle("hide");
   gameCard.classList.toggle("hide");
 }
 easy.addEventListener("click", (event) => {
-  guessInput.value="";
+  guessInput.value = "";
   playGame();
   playGameEasy();
-  });
+});
+async function judge() {
+  gameCard.classList.toggle("hide");
+  judgement.classList.toggle("hide");
+}
 //pop option to play again with yes or no buttons (modal?)
