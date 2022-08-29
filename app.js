376bde02e3b98ea520328a53927d1608a7442593
TheBigH50 <HFangmeier@outlook.com>
2022-08-29 07:32:42 -0500

    start setup of easy mode

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 //welcome screen/start game (main screen, pose question give 2 buttons yes or no)
+let makeGuess = document.getElementById("makeGuess");
+let guessInput = document.getElementById("guessInput");
+let gameCardLabel = document.getElementById("gameCardLabel");
 let startGame = document.getElementById("start");
 let noThanks = document.getElementById("noThanks");
 let thankYou = document.getElementById("thankYouCard");
 let wouldYou = document.getElementById("wouldYou");
 let gameCard = document.getElementById("gameCard");
+let difficultyOption = document.getElementById("difficultyOption");
+let easy = document.getElementById("easy");
+let hard = document.getElementById("hard");
+let overkill = document.getElementById("overkill");
 //make no pop to thanks for visiting page
-function hideContainers() {
+async function hideContainers() {
   wouldYou.classList.toggle("hide");
   thankYou.classList.toggle("hide");
 }
@@ -15,7 +22,7 @@ noThanks.addEventListener("click", (event) => {
 });
 
 //make yes pose difficulty choice with 4 buttons
-function startDifficulty() {
+async function startDifficulty() {
   wouldYou.classList.toggle("hide");
   difficultyOption.classList.toggle("hide");
 }
@@ -24,4 +31,33 @@ startGame.addEventListener("click", (event) => {
 });
 //connect choice to game of choice
 
+async function playGameEasy() {
+  var rand = Math.floor(Math.random() * 10) + 1;
+  gameCardLabel.innerText=`I'm Thinking of a Number from 1 to 10...`;
+  let guess;
+  do {
+    guess = guessInput.value;
+    if (guess < rand) {
+      alert(`${guess} is too low`);
+    } else if (guess > rand) {
+      alert(`${guess} is too high`);
+    } else if (isNaN(guess)) {
+      alert(`Guess a number please`);
+    } else {
+      alert(`${guess} IS CORRECT!`);
+      console.log(
+        `WAY TO GO YOU GUESSED THE NUMBER ${guess} YOU ARE SO SMART!`
+      );
+    }
+  } while (rand != guess);
+}
+async function playGame() {
+  difficultyOption.classList.toggle("hide");
+  gameCard.classList.toggle("hide");
+}
+easy.addEventListener("click", (event) => {
+  guessInput.value="";
+  playGame();
+  playGameEasy();
+  });
 //pop option to play again with yes or no buttons (modal?)
